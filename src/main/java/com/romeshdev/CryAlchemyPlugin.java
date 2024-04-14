f6b7fc061216e1bc1cac8ae77ce3fe79509d3f86
Romesh <romesh@leagueofgeeks.com>
2024-04-14 14:51:25 +1000

    initial

---
+++ src/main/java/com/romeshdev/CryAlchemyPlugin.java
@@ -0,0 +1,34 @@
+package com.romeshdev;
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.events.GraphicChanged;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+
+@PluginDescriptor(
+        name = "Cry Alchemy",
+        description = "Swaps the default alchemy animations with the cry emote",
+        tags = {"alch","alchemy","cry","animation","swap"}
+)
+@Slf4j
+public class CryAlchemyPlugin extends Plugin {
+
+    private static final int CRY_EMOTE_ANIMATION_ID = 860;
+    private static final int LOW_LEVEL_ALCHEMY_GFX_ID = 112;
+    private static final int HIGH_LEVEL_ALCHEMY_GFX_ID = 113;
+    private static final int ALCHEMY_GFX_HEIGHT_OFFSET = 100;
+
+    @Subscribe
+    public void onGraphicChanged(GraphicChanged graphicChange)
+    {
+        var actor = graphicChange.getActor();
+        var graphic = actor.getGraphic();
+        if(graphic == LOW_LEVEL_ALCHEMY_GFX_ID || graphic == HIGH_LEVEL_ALCHEMY_GFX_ID)
+        {
+            actor.setAnimation(CRY_EMOTE_ANIMATION_ID);
+            actor.setGraphic(LOW_LEVEL_ALCHEMY_GFX_ID);
+            actor.setGraphicHeight(ALCHEMY_GFX_HEIGHT_OFFSET);
+            actor.setSpotAnimFrame(0);
+        }
+    }
+}
---
+++ src/test/java/com/romeshdev/PluginLauncher.java
@@ -0,0 +1,13 @@
+package com.romeshdev;
+
+import net.runelite.client.RuneLite;
+import net.runelite.client.externalplugins.ExternalPluginManager;
+
+public class PluginLauncher
+{
+	public static void main(String[] args) throws Exception
+	{
+		ExternalPluginManager.loadBuiltin(CryAlchemyPlugin.class);
+		RuneLite.main(args);
+	}
+}
